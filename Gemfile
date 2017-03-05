a89e6650300a395c0622fbeb146ab28f43dcd679
Kip Kosek <kipkosek@gmail.com>
2017-03-04 21:26:24 -0500

    US1: Add Sign Up and Sign In

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'turbolinks'
 # gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap'
+gem 'devise'
 gem 'figaro'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: :index
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
